707c890ccae30d37457dd363f82eccde40bb2322
svyrydenko <svyrydenko@netcracker.com>
2019-11-01 19:30:51 +0200

    First push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PageComponent} from './page/page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {
+    path: 'home', component: PageComponent, data: {
+      page: 'home'
+    }
+  },
+  {
+    path: 'about', component: PageComponent, data: {
+      page: 'about'
+    }
+  },
+  {
+    path: 'contact', component: PageComponent, data: {
+      page: 'contact'
+    }
+  },
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PageComponent} from './page/page.component';
+import {FullpageDirective} from './shared/directives/fullpage.directive';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatSliderModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {ContentService} from './shared/services/content.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    FullpageDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTabsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [ContentService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
